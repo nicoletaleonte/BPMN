9a0c20aff3c02a482c93ccd967ce887ee306e55b
nicol <nicoletaleonte24@gmail.com>
2020-05-19 20:19:52 +0300

    Activity project, BPMN diagram, one error

---
+++ src/main/java/pachetel/CalculareSuma.java
@@ -0,0 +1,11 @@
+package pachetel;
+import org.activiti.engine.delegate.*;
+
+public class CalculareSuma implements JavaDelegate {
+
+	public void execute(DelegateExecution arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/pachetel/Comanda.java
@@ -0,0 +1,7 @@
+package pachetel;
+
+import java.io.Serializable;
+
+public class Comanda implements Serializable {
+
+}
